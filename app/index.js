d991070d9df50b6fd28245260154f2535caa91e1
Imoh Precious <jaypee88830@gmail.com>
2024-08-09 04:45:13 +0100

    upload image works

--- app/index.js
+++ app/index.js
@@ -1,13 +1,27 @@
 // App.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, FlatList, Alert, Image } from 'react-native';
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity, Alert, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import Product from '../components/Product';
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [name, setName] = useState('');
-  const [photo, setPhoto] = useState('');
+  const [photo, setPhoto] = useState(null);
   const [price, setPrice] = useState('');
+  
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPhoto(result.assets[0].uri);
+    }
+  };
 
   const addProduct = () => {
     if (products.length >= 5) {
@@ -21,6 +35,7 @@ const App = () => {
     setPrice('');
   };
 
+
   return (
     <View className='flex-1 p-4 bg-gray-100'>
       <TextInput
@@ -29,12 +44,21 @@ const App = () => {
         value={name}
         onChangeText={setName}
       />
-      <TextInput
+      {/*<TextInput
         className='border p-2 mb-2'
         placeholder="Product Photo URL"
         value={photo}
         onChangeText={setPhoto}
-      />
+      />*/}
+      <TouchableOpacity onPress={pickImage} className="border p-2 mb-2 bg-white justify-center items-center">
+        <Text>{photo ? "Change Photo" : "Upload Photo"}</Text>
+      </TouchableOpacity>
+      {photo && (
+        <Image
+          source={{ uri: photo }}
+          className='h-40 w-full rounded-lg mb-2'
+        />
+      )}
       <TextInput
         className='border p-2 mb-2'
         placeholder="Product Price"
